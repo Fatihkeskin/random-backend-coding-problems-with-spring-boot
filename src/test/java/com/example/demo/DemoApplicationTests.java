79df7bb23ed6c5a37ff4e27be5bfc743a0aaa4e4
Fatih Keskin <fatihkeskin7@outlook.com>
2022-01-31 22:19:04 +0300

    junit and mockito tryouts
    test cases written using junit and mockito

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,56 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
+import com.example.demo.student.StudentRepository;
+import com.example.demo.student.StudentService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 class DemoApplicationTests {
 
+	@Mock
+	StudentRepository studentRepository;
+
+	@InjectMocks
+	private StudentService studentService;
+
+	@DisplayName("Should pass a non-null message to our test method")
+	@ParameterizedTest(name = "{index} => message=''{0}''")
+	@ValueSource(strings = {"Hello", "World"})
+	void shouldPassNonNullMessageAsMethodParameter(String message) {
+		assertNotNull(message);
+	}
+
+	@DisplayName("Should calculate the correct sum")
+	@ParameterizedTest(name = "{index} => a={0}, b={1}, sum={2}")
+	@CsvSource({
+			"1, 1, 2",
+			"2, 3, 5"
+	})
+	void sum(int a, int b, int sum) {
+		assertEquals(sum, a + b);
+	}
+
 	@Test
-	void contextLoads() {
+	public void deleteStudentWithNoIDThrowsException(){
+		Student student = new Student("name", "email@gmail.com", LocalDate.of(2006, Month.JANUARY, 5));
+		assertThrows(NullPointerException.class,
+				()->{
+					studentService.deleteStudent(student.getId());
+				});
 	}
 
 }
